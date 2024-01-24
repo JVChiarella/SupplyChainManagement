f82801eafab93434d54ef4702e43e7af03c3ed4d
Jason Chiarella <jvc2n@mtmail.mtsu.edu>
2024-01-24 11:46:07 -0600

    updating services

---
+++ supply_chain_management_frontend/src/types/types.d.ts
@@ -0,0 +1,54 @@
+type Order = {
+    id? : number;
+    customer : Customer;
+    invoice : Invoice;
+    orderedItems : OrderedItem;
+    timestamp : Date;
+}
+
+type UserCredentials = {
+    username: string;
+    password: string;
+}
+
+type Invoice = {
+    id? : number;
+    order : Order;
+    customerID : number;
+    totalPrice : number;
+}
+
+type OrderedItem = {
+    id? : number;
+    stockID : number;
+    amount : number;
+}
+
+type StockItem = {
+    id? : number;
+    count : number;
+    price : number;
+}
+
+type Customer = {
+    id?: number;
+    credentials: UserCredentials;
+    firstName: string;
+    lastName: string;
+    email: string;
+    address : string;
+    phoneNumber: string;
+    active: boolean;
+    orders?: Order[];
+};
+
+type Employee = {
+    id?: number;
+    credentials: UserCredentials;
+    firstName: string;
+    lastName: string;
+    email: string;
+    admin: boolean;
+    active: boolean;
+    orders?: Order[];
+};
