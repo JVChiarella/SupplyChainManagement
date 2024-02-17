706816d25abbad73cec3a1f9f39c55da8301f6ed
Jason Chiarella <jvc2n@mtmail.mtsu.edu>
2024-02-17 11:24:13 -0600

    working on add order component -- the final basic functionality of app needing to be implemented

--- supply_chain_management_frontend_nextjs/src/app/components/AddOrder.tsx
+++ supply_chain_management_frontend_nextjs/src/app/components/AddOrder.tsx
@@ -1,19 +1,43 @@
 'use client'
-import React, { useState, FormEvent } from 'react'
+import React, { useState, useEffect } from 'react'
 
-//WIP
-const NewOrder = () => {
+//WIP -> need to be able to add stock to ordered items array of newOrder + set state to submit to API
+const AddOrder = () => {
 
-    const [ postComplete, setPostComplete ] = useState(false)
+    const defaultOrder : Order = {customer_id : 0,
+                                  invoice : {status : "", totalPrice : 0},
+                                  ordered_items : [],
+                                  date : new Date(0)}
+    const defaultStock : StockItem[] = [];
 
-    async function handleNewOrderSubmit(event: FormEvent<HTMLFormElement>){
-        event?.preventDefault()
-        const formData = new FormData(event.currentTarget)
-        const items = formData?.get('items')
+    const [ postComplete, setPostComplete ] = useState(false);
+    const [ gotStock, setGotStock ] = useState(false);
+    const [ stock, setStock ] = useState(defaultStock)
+    const [ newOrder, setNewOrder ] = useState(defaultOrder);
+    const [ error, setError] = useState(false)
+        
+    useEffect(() => {
+        const getData = async () => {
+            //fetch data from spring api
+            const data : StockItem[] | any = await getStock();
 
+            if(data['message']){
+                //handle error
+                setError(true)
+            } else {           
+                //update state and return
+                setStock(data);
+                setGotStock(true);
+                return
+            }
+        };
+        getData();
+    }, []);
+
+    async function handleNewOrderSubmit(order : Order){
         //create necessary body to send to api for request
         const endpoint = `orders/new`
-        const payload = ''
+        const payload = order
 
         const res = await fetch('/api/crud/post', {
           method: 'POST',
@@ -31,31 +55,83 @@ const NewOrder = () => {
 
     if(postComplete){
         return (
-            <div className="list-container">
-                <div>-----------------------</div>
-                <h3>Place a New Order:</h3>
-
-                <form onSubmit={handleNewOrderSubmit}>
-                    <input type="text" name="items" placeholder="Items" required/>
-                    <button className='submit-button' type="submit">Add</button>
-                </form>
-
+            <div className="crud-items">
+                <div className='subtitle'>Place a New Order</div>
+                <div className='table-container'>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Count</th>
+                                <th>Price</th>
+                            </tr>
+                            {stock.map(item => 
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.description}</td>
+                                    <td>{item.count}</td>
+                                    <td>{item.price}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+                <button className='submit-button' onClick={()=> handleNewOrderSubmit(newOrder)}>Submit</button>
                 <h1>order placed successfully!</h1>
             </div>
         )
-    } else {
+    } else if(gotStock){
         return (
-            <div className="list-container">
-                <div>-----------------------</div>
-                <h3>Place a New Order:</h3>
-
-                <form onSubmit={handleNewOrderSubmit}>
-                    <input type="text" name="items" placeholder="Items" required/>
-                    <button className='submit-button' type="submit">Add</button>
-                </form>
+            <div className="crud-items">
+                <div className='subtitle'>Place a New Order</div>
+                <div className='table-container'>
+                <table>
+                        <tbody>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Count</th>
+                                <th>Price</th>
+                            </tr>
+                            {stock.map(item => 
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.description}</td>
+                                    <td>{item.count}</td>
+                                    <td>{item.price}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+                <button className='submit-button' onClick={()=> handleNewOrderSubmit(newOrder)}>Submit</button>
             </div>
         )
+    } else if(error){
+        return (
+            <div>
+                --An Error Occured While Loading Stock--
+            </div>
+        )
+    } else {
+        <div>Loading...</div>
     }
 }
 
-export default NewOrder
+async function getStock(){
+    const res = await fetch('/api/crud/get', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ endpoint: "stock"})
+    });
+  
+    const users = await res.json();
+    return users;
+}
+
+export default AddOrder
--- supply_chain_management_frontend_nextjs/src/app/orders/page.tsx
+++ supply_chain_management_frontend_nextjs/src/app/orders/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import GetAllOrdersByCustomer from '../components/GetAllOrdersByCustomer'
+import AddOrder from '../components/AddOrder'
 
 const OrdersPage = () => {
   return (
     <div>
         <GetAllOrdersByCustomer></GetAllOrdersByCustomer>
+        <AddOrder></AddOrder>
     </div>
   )
 }
